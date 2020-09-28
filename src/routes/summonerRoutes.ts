7332140cf34dbce15fe1762ae35f5cd0c9bfc773
FabioCeleste <36655978+FabioCeleste@users.noreply.github.com>
2020-09-28 09:08:27 -0300

    delete summon delete -> post

--- src/routes/summonerRoutes.ts
+++ src/routes/summonerRoutes.ts
@@ -6,7 +6,7 @@ import loginRequired from '../middlewares/loginRequired'
 const router = Router()
 
 router.post('/', loginRequired, summonerController.store)
-router.delete('/', loginRequired, summonerController.delete)
+router.post('/delete', loginRequired, summonerController.delete)
 router.post('/favs', loginRequired, summonerController.index)
 
 export default router
