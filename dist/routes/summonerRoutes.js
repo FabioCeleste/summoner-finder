45973e58b796dde346152e68be411c6bfed59ce5
FabioCeleste <36655978+FabioCeleste@users.noreply.github.com>
2020-09-28 09:09:22 -0300

    delete summoner method delete -> post

--- dist/routes/summonerRoutes.js
+++ dist/routes/summonerRoutes.js
@@ -8,7 +8,7 @@ const SummonerController_1 = __importDefault(require("../controllers/SummonerCon
 const loginRequired_1 = __importDefault(require("../middlewares/loginRequired"));
 const router = express_1.Router();
 router.post('/', loginRequired_1.default, SummonerController_1.default.store);
-router.delete('/', loginRequired_1.default, SummonerController_1.default.delete);
+router.post('/delete', loginRequired_1.default, SummonerController_1.default.delete);
 router.post('/favs', loginRequired_1.default, SummonerController_1.default.index);
 exports.default = router;
 //# sourceMappingURL=summonerRoutes.js.map
